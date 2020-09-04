9172bf80c31f4abe5685c602adbd701a1052a0ac
ramang22 <wibrach@gmail.com>
2020-09-04 09:50:32 +0200

    api key setup

--- server/main.py
+++ server/main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
+
+# http://127.0.0.1:5000/?player1=orth&server1=eune&player2=willko&server2=eune
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    playerOne = request.args.get('player1')
+    playerTwo = request.args.get('player2')
+    serverOne = request.args.get('server1')
+    serverTwo = request.args.get('server2')
+    return playerOne+serverOne+playerTwo+serverTwo+"kapp"
