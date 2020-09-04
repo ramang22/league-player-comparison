951d4681c7c7cb92387d5056c3f8d59c9bc2399d
Samuel <samuelorth62@gmail.com>
2020-09-04 11:17:55 +0200

    created form

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,54 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Summoner Vision</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <form class="custom_form">
+          <div class="form-row">
+            <div class="col-sm-6">
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <select class="btn btn-outline-info dropdown-toggle" required>
+                    <option value="EUW1">EUW</option>
+                    <option value="EUN1">EUNE</option>
+                    <option value="KR">KR</option>
+                    <option value="NA1">NA</option>
+                    <option value="BR1">BR</option>
+                    <option value="JP1">JP</option>
+                    <option value="LA1">LAN</option>
+                    <option value="LA2">LAS</option>
+                    <option value="OC1">OC</option>
+                    <option value="RU">RU</option>
+                    <option value="TR1">TR</option>
+                  </select>
+                </div>
+                <input type="text" class="form-control" aria-label="Text input with dropdown button" placeholder="Summoner Name" required></input>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <select class="btn btn-outline-info dropdown-toggle" required>
+                    <option value="EUW1">EUW</option>
+                    <option value="EUN1">EUNE</option>
+                    <option value="KR">KR</option>
+                    <option value="NA1">NA</option>
+                    <option value="BR1">BR</option>
+                    <option value="JP1">JP</option>
+                    <option value="LA1">LAN</option>
+                    <option value="LA2">LAS</option>
+                    <option value="OC1">OC</option>
+                    <option value="RU">RU</option>
+                    <option value="TR1">TR</option>
+                  </select>
+                </div>
+                <input type="text" class="form-control" aria-label="Text input with dropdown button" placeholder="Summoner Name" required></input>
+              </div>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-dark">Compare</button>
+        </form>
       </div>
     );
   }
