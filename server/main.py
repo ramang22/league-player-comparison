a711ea7baeae4f7903eafe4118131dd3cd5256aa
ramang22 <wibrach@gmail.com>
2020-09-07 12:31:27 +0200

    adding flask abort calls

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 from flask import request
 from ApiWorker import ApiHandler
 from flask_cors import CORS
@@ -7,9 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# http://127.0.0.1:5000/?player1=orth&server1=eune&player2=willko&server2=eune
 @app.route('/')
 def hello_world():
+    
     playerOne = request.args.get('player1')
     playerTwo = request.args.get('player2')
     serverOne = request.args.get('server1')
@@ -17,5 +17,17 @@ def hello_world():
     flexq = request.args.get('flexq')
     soloq = request.args.get('soloq')
     num = request.args.get('numOfGames')
+
+
+    if playerOne is None or playerOne == "" :
+        abort(404, description="Player 1 not found")
+    if playerTwo is None or playerTwo == "":
+        abort(404, description="Player 2 not found")
+    if flexq is None:
+        flexq = False
+    if soloq is None:
+        soloq = True
+    if num is None:
+        num = 20
     response = ApiHandler.getStats(playerOne, serverOne, playerTwo, serverTwo, flexq, soloq, num)
     return response
